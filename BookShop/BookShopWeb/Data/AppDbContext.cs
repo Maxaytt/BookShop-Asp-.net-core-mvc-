693593b9665577b436e2cfae88a9aa966bff22c8
Maxaytt <maxaytt@gmail.com>
2022-11-27 16:56:01 +0100

    f
    f

---
+++ BookShop/BookShopWeb/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookShopWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
